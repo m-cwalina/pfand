78a73e9b51c0d39af16d255e4ab307e54dc684a4
Matthew Cwalina <matthewcwalina@gmail.com>
2022-04-21 18:51:58 +0200

    Changed devise log in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { host: ENV["DOMAIN"] || "www.pfandme.net" }
   end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:donor])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:donor])
+  end
 end
